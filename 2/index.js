6f762cc59ba344abc4a4ac6ed72a5fcebcbb5cc1
Basit Ahmed <basitahmed@live.com>
2023-02-21 17:59:34 +0500

    Add files via upload

---
+++ 2/index.js
@@ -0,0 +1,3 @@
+var personName = "Basit Ahmed";
+var msg = "Hello ".concat(personName, ", would you like to learn some Typescript today?");
+console.log(msg);
---
+++ 2/index.ts
@@ -0,0 +1,3 @@
+let personName = "Basit Ahmed";
+let msg = `Hello ${personName}, would you like to learn some Typescript today?`
+console.log(msg);
